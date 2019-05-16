a6b91a0ded52f635da21c37403b7c8238132579a
emelkov <evm2772@gmail.com>
2019-05-16 12:21:11 +0300

    fix textures plugin

--- dkfx/plugins/sync_textures.py
+++ dkfx/plugins/sync_textures.py
@@ -18,26 +18,26 @@ import fnmatch
 import re
 
 
-def textures_parser(root_folder, logger):
-    masks = ["*.bmp", "*.exr", "*.gif", "*.hdri", "*.jpeg", "*.jpg", "*.png", "*.psd", "*.tiff", "*.tif", "*.tga"]
-    includes = '|'.join([fnmatch.translate(x) for x in masks])
-    texture_paths = []
-    for (dirpath, dirnames, filenames) in os.walk(root_folder):
-        if dirnames:
-            # find versions:
-            version_dirs = [d for d in dirnames if re.match(r'v\d\d', d)]
-            if version_dirs:
-                version_dirs.sort() # for any case ;-)
-                last_version_dir = os.path.join(dirpath, version_dirs[-1])
-                logger.debug('Last version dir: {0} --> {1}'.format(version_dirs, last_version_dir))
-                for (dirpath, dirnames, filenames) in os.walk(last_version_dir):
-                    files = [f for f in filenames if re.match(includes, f)]
-                    logger.debug ('Found files: %s' % str(files))
-                    for f in files:
-                        texture_paths.append(os.path.join(last_version_dir, f))
-
-    logger.debug('Files for sync: \n %s' % pformat(texture_paths))
-    return texture_paths
+# def textures_parser(root_folder, logger):
+#     masks = ["*.bmp", "*.exr", "*.gif", "*.hdri", "*.jpeg", "*.jpg", "*.png", "*.psd", "*.tiff", "*.tif", "*.tga"]
+#     includes = '|'.join([fnmatch.translate(x) for x in masks])
+#     texture_paths = []
+#     for (dirpath, dirnames, filenames) in os.walk(root_folder):
+#         if dirnames:
+#             # find versions:
+#             version_dirs = [d for d in dirnames if re.match(r'v\d\d', d)]
+#             if version_dirs:
+#                 version_dirs.sort() # for any case ;-)
+#                 last_version_dir = os.path.join(dirpath, version_dirs[-1])
+#                 logger.debug('Last version dir: {0} --> {1}'.format(version_dirs, last_version_dir))
+#                 for (dirpath, dirnames, filenames) in os.walk(last_version_dir):
+#                     files = [f for f in filenames if re.match(includes, f)]
+#                     logger.debug ('Found files: %s' % str(files))
+#                     for f in files:
+#                         texture_paths.append(os.path.join(last_version_dir, f))
+#
+#     logger.debug('Files for sync: \n %s' % pformat(texture_paths))
+#     return texture_paths
 
 def registerCallbacks(reg):
     """
@@ -91,13 +91,63 @@ def sync_textures(sg, logger, event, args):
     :param logger: Logger instance.
     :param event: A Shotgun EventLogEntry entity dictionary.
     :param args: Any additional misc arguments passed through this plugin.
+
+    event ex.: {'attribute_name': 'sg_status_list',
+             'created_at': datetime.datetime(2019, 5, 16, 12, 7, 12, tzinfo=<shotgun_api3.lib.sgtimezone.LocalTimezone object at 0x7fa4aef43910>),
+             'entity': {'id': 29343, 'name': 'txtr', 'type': 'Task'},
+             'event_type': 'Shotgun_Task_Change',
+             'id': 2949503,
+             'meta': {'attribute_name': 'sg_status_list',
+                      'entity_id': 29343,
+                      'entity_type': 'Task',
+                      'field_data_type': 'status_list',
+                      'new_value': 'cmpt',
+                      'old_value': 'hld',
+                      'type': 'attribute_change'},
+             'project': {'id': 92, 'name': 'SOUZ_S', 'type': 'Project'},
+             'session_uuid': 'd4e4e4b0-7730-11e9-90b5-0242ac110004',
+             'type': 'EventLogEntry',
+             'user': {'id': 198, 'name': 'Evgeniy Melkov', 'type': 'HumanUser'}
+             }
+
     """
 
-    task_name = event['entity']['name']
-    new_status = event['meta']['new_value']
-    old_status = event['meta']['old_value']
-    project_id = event['project']['id'] #{'id': 92, 'name': 'SOUZ_S', 'type': 'Project'}
-    #logger.warning('!!Warning!! working for project "SOUZ_S" (id = %s) only!!' % project_id)
+
+
+
+    #
+    # field_name = event.get("attribute_name")
+    # entity = event.get("entity")
+    # project = event.get("project")
+    # new_value = event.get("meta", {}).get("new_value")
+    # old_value = event.get("meta", {}).get("new_value")
+    # user = event.get("user")
+    # event_id = event.get("id")
+
+
+    entity = event.get("entity")
+    project = event.get("project")
+    user = event.get("user")
+    meta = event.get("meta")
+
+    # Make sure all our event keys contain values.
+    if None in [entity, project, user, meta]:
+        logger.warning("Missing info in event dictionary, skipping.")
+        return
+
+
+    if user['id'] != 198:
+        logger.warning('################################# Not developer user. Skipping')
+        return
+    else:
+        logger.warning('BINGO! DEVELOPER EVM ... ')
+
+
+    task_name = entity.get('name')
+    new_status = meta.get('new_value')
+    old_status = meta.get('old_value')
+    project_id = project.get('id')  # {'id': 92, 'name': 'SOUZ_S', 'type': 'Project'}
+
     logger.debug("%s status: %s --> %s" % (event['entity'], old_status, new_status))
 
     if task_name == 'txtr' and new_status == 'cmpt':
@@ -162,14 +212,14 @@ def sync_textures(sg, logger, event, args):
                 logger.error('"Storage" textures directory not found: %s' % storage_textures_dir)
                 return
             
-            files = textures_parser(storage_textures_dir, logger)
+            #files = textures_parser(storage_textures_dir, logger)
         else:
             logger.error('No filesystem location. Must be corrected.')
             return
 
         dst_log = os.path.join(panasas_textures_dir, 'auto_export.log')
-        cmd = os.path.normpath(os.path.join(os.path.dirname(__file__), '../scripts/sync_textures.sh %s %s %s > %s' %
-                                            (storage_textures_dir, panasas_textures_dir, project_id, dst_log)))
+        # cmd = os.path.normpath(os.path.join(os.path.dirname(__file__), '../scripts/sync_textures.sh %s %s %s > %s' %
+        #                                     (storage_textures_dir, panasas_textures_dir, project_id, dst_log)))
 
         # debug:
         cmd = os.path.normpath(os.path.join(os.path.dirname(__file__), '../scripts/sync_textures.sh %s %s %s 2>&1 | tee %s' %
